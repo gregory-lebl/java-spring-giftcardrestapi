952712320be69afdbd3e0693c786dd1c920fcd61
Gregory <johndoe@example.com>
2025-09-19 20:18:08 +0200

    chore: add tables sql

---
+++ src/main/resources/setup/tables.sql
@@ -0,0 +1,33 @@
+create table public."user"
+(
+    "id"         UUID    not null default gen_random_uuid(),
+    "first_name" varchar not null,
+    "last_name"  varchar not null,
+    "email"      varchar not null,
+    "password"   varchar not null,
+    "created_at" date    not null default now(),
+    "updated_at" date    not null default now(),
+    primary key ("id")
+);
+
+create table public."company"
+(
+    "id"         uuid    not null default gen_random_uuid(),
+    "name"       varchar not null,
+    "siren"      varchar not null,
+    "created_at" date    not null default now(),
+    "updated_at" date    not null default now(),
+    primary key ("id")
+);
+
+create table public."card"
+(
+    "id"         uuid    not null default gen_random_uuid(),
+    "user_id"    uuid    not null references "user" (id),
+    "company_id" uuid    not null references "company" (id),
+    "amount"     integer not null,
+    "expire_at"  date    not null,
+    "created_at" date    not null default now(),
+    "updated_at" date    not null default now(),
+    primary key ("id")
+);
